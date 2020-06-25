b6a905a483c88a186199cfb2b619108cd9e81cca
ionut22 <ionut.vilceanu22@gmail.com>
2020-06-25 15:41:57 +0300

    da

---
+++ Store/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalaJocuriLicenta.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public int ProductPrice { get; set; }
+        public string AvatarProduct { get; set; }
+        public int ProductHours { get; set; }
+
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+        
+        public virtual ICollection<ProductOrder> ProductOrders { get; set; }
+        public virtual ICollection<Favorit> Favorits { get; set; }
+        public virtual ICollection<ShoppingCart> ShoppingCarts { get; set; }
+
+
+    }
+}
---
+++ Store/Models/ProductOrder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalaJocuriLicenta.Models
+{
+    public class ProductOrder
+    {
+        public int ProductOrderId{get; set;}
+        public int OrderId { get; set; }
+
+
+    }
+}
